b8a927158a64f6ac41c8d095dc602faa776b44e4
yaoxf <850630887@qq.com>
2022-06-19 12:54:18 +0800

    md

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aplum/proto',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'bd96ec253e88b8128a1e9cd7255dd655d763b988',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aplum/proto' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'bd96ec253e88b8128a1e9cd7255dd655d763b988',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
