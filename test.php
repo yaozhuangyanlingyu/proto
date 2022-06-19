317cbcd3ce973ad5ac78d90ccf98c5b6c6bda45e
yaoxf <yaoxiaofeng@aplum.onaliyun.com>
2022-06-19 17:58:12 +0800

    test

--- test.php
+++ test.php
@@ -4,14 +4,13 @@ require_once __DIR__ . '/vendor/autoload.php';
 use aplum_proto\Tservice;
 use app\components\proto\order\SvoucherExpireTimeRequest;
 
-// 测试3333
+// 测试1758
 function test1() {
     $obj = new Tservice();
     $time=$obj->getDate();
     echo $time . "\n";
 }
 
-// 测试4444
 function test2() {
     $obj = new SvoucherExpireTimeRequest();
     echo "<pre>";
