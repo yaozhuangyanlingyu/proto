3b4403d5d6452aa5bd5be6b828792be34cac56ff
yaoxf <yaoxiaofeng@aplum.onaliyun.com>
2022-06-19 13:04:51 +0800

    add test

---
+++ src/Tservice.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Aplum\Proto;
+
+class Tservice {
+	public function __construct(){
+
+	}
+
+	public function getDate(){
+		return date('Y-m-d H:i:s');
+	}
+
+	/**
+     * tag 1.1
+     */
+    public function getDatev2(){
+        return date('Y-m-d H-i-s');
+    }
+}
+
